a2bbf4e881299ae85643ac798c6edaf01048ff70
RoseGladson <RoseGladson@IBM-PF2ZCV8A>
2025-02-13 11:54:49 +0530

    Add project files.

---
+++ DSAPractice/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] array = new int[3];
+array[0] = 1;
+array[1] = 2;
+array[2] = 3;
+
+
+Array.ForEach(array, Console.WriteLine);
+
+Console.ReadKey();
+
+
